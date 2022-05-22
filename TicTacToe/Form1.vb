1ef383015c2eebd6e9156856ef12385bd777565e
jacobasuncion <asuncion1173@gmail.com>
2022-05-22 18:57:40 +0800

    Coded btn_Click to register X and O alternate

--- TicTacToe/Form1.vb
+++ TicTacToe/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Dim countx As Integer = 0
-    Dim turn As Boolean = 0
+    Dim turn As Boolean = True
     Dim player1Score As Short = 0
     Dim player2Score As Short = 0
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -10,5 +10,15 @@
 
     Private Sub btn_Click(sender As Object, e As EventArgs) Handles btnTopRight.Click, btnTopMid.Click, btnTopLeft.Click, btnMidRight.Click, btnMidMid.Click, btnMidLeft.Click, btnBotRight.Click, btnBotMid.Click, btnBotLeft.Click
         Dim x As Button = CType(sender, Button)
+        If turn Then
+            x.Text = "X"
+            countx += 1
+            turn = False
+        Else
+            x.Text = "O"
+            turn = True
+        End If
+        x.Enabled = False
+
     End Sub
 End Class
